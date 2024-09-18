8065b4c274db843fd6672d671de7881f16d7dafd
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-09-18 04:10:08 +0200

    Se adelanta hoja de ejercicios 2 de JavaScript

---
+++ desarrollo-web-entorno-cliente/unidad-2/tareas/hoja-ejercicios-2/ejercicio08.js
@@ -0,0 +1,7 @@
+/* Escribir una función que busque la cadena "arriba" en un texto. La función 
+deberá informar si encuentra o no esta cadena en el texto y, si la encuentra, nos deberá 
+mostrar el texto reemplazando "arriba" por "abajo". */
+
+function replaceArriba(text) {
+    
+}
