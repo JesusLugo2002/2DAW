8eee1e6ee616d6dbb493e4cdbb7869e286acc837
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-24 09:02:47 +0100

    Se corrige condición de tarea 5 de jQuery

--- desarrollo-web-entorno-cliente/unidad-3/tareas/tarea05/script.js
+++ desarrollo-web-entorno-cliente/unidad-3/tareas/tarea05/script.js
@@ -1,54 +1,60 @@
-// Función para agregar tareas
-$("#agregar-tarea").click(function() {
-    const new_task = $("#nueva-tarea").val()
-    $("#lista-tareas").append(getTask(new_task));   
-    bindEvents()
-});
-
-// Función que retorna una nueva tarea a raiz de su nombre
-function getTask(name) {
-    html = `
-    <li>
-        <p>${name}</p>
-        <div style="display: flex;">
-            <button class="edit-button">Edit</button>
-            <button class="delete-button">Delete</button>
-        </div>
-    </li>
-    `
-    return html
-}
-
-// Funcionalidad para los botones de Edit y Delete
-function bindEvents() {
-    $(".delete-button").click(function() {
-        $(this).parents("li").remove();
+$(document).ready(function() {
+    // Función para agregar tareas
+    $("#agregar-tarea").click(function() {
+        const new_task = $("#nueva-tarea").val()
+        if (new_task != "") {
+            $("#lista-tareas").append(getTask(new_task));   
+            bindEvents()
+        } else {
+            alert("Nombre de la misión requerida!")
+        }
     });
 
-    $(".edit-button").click(function() {
-        const item = $(this).parents("li")
-        $(".selected-edit").removeClass("selected-edit")
-        $(item).addClass("selected-edit")
-        toggleModal()
-    });
-}
+    // Función que retorna una nueva tarea a raiz de su nombre
+    function getTask(name) {
+        html = `
+        <li>
+            <p>${name}</p>
+            <div style="display: flex;">
+                <button class="edit-button">Edit</button>
+                <button class="delete-button">Delete</button>
+            </div>
+        </li>
+        `
+        return html
+    }
 
-// Funcionalidad del modal
-function toggleModal() {
-    $("#blur-background").show();
-    $("#toggle-modal").show();
-    $("#toggle-modal #confirm-button").click(function() {
-        $(".selected-edit").find("p").text($("#toggle-modal input").val());
-        $("#toggle-modal").hide();
-        $("#blur-background").hide();
-    });
-    $("#toggle-modal #cancel-button").click(function() {
-        $("#toggle-modal").hide();
-        $("#blur-background").hide();
-    });
-}
+    // Funcionalidad para los botones de Edit y Delete
+    function bindEvents() {
+        $(".delete-button").click(function() {
+            $(this).parents("li").remove();
+        });
 
-// Función "borrar todas las tareas"
-$("#limpiar-tareas").click(function() {
-    $("#lista-tareas").empty()
+        $(".edit-button").click(function() {
+            const item = $(this).parents("li")
+            $(".selected-edit").removeClass("selected-edit")
+            $(item).addClass("selected-edit")
+            toggleModal()
+        });
+    }
+
+    // Funcionalidad del modal
+    function toggleModal() {
+        $("#blur-background").show();
+        $("#toggle-modal").show();
+        $("#toggle-modal #confirm-button").click(function() {
+            $(".selected-edit").find("p").text($("#toggle-modal input").val());
+            $("#toggle-modal").hide();
+            $("#blur-background").hide();
+        });
+        $("#toggle-modal #cancel-button").click(function() {
+            $("#toggle-modal").hide();
+            $("#blur-background").hide();
+        });
+    }
+
+    // Función "borrar todas las tareas"
+    $("#limpiar-tareas").click(function() {
+        $("#lista-tareas").empty()
+    });
 });
