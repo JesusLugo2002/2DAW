829d5b9ef51cc23f164451d8815dba64a812ec56
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-28 16:04:40 +0000

    Se añade parte del ejercicio 1 de ts

---
+++ desarrollo-web-entorno-cliente/unidad-4/tareas/tarea01/src/script.ts
@@ -0,0 +1,14 @@
+// Ejercicio D1S1.1.1
+let msg: string = 'Hola mundo'
+$("#output1").text(msg)
+  
+// Ejercicio D1S1.1.2
+
+
+// Ejercicio D1S2.1.1
+
+// Ejercicio D1S2.1.2
+
+// Ejercicio D1S2.2.1
+
+// Ejercicio D1S2.2.2
