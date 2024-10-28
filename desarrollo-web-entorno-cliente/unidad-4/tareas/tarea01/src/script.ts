a3214c12bb1c9dfe9aa2e07a227ae135f153da53
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-28 17:00:55 +0000

    Se termina tarea de TypeScript

--- desarrollo-web-entorno-cliente/unidad-4/tareas/tarea01/src/script.ts
+++ desarrollo-web-entorno-cliente/unidad-4/tareas/tarea01/src/script.ts
@@ -3,12 +3,57 @@ let msg: string = 'Hola mundo'
 $("#output1").text(msg)
   
 // Ejercicio D1S1.1.2
-
+type Freddy = {
+    name: string;
+    age: number;
+    nightmare: string;
+}
+let osoPendejo: Freddy = {name: "Freddy", age: 1.5, nightmare: "Color perú"}
+$("#output2").text(JSON.stringify(osoPendejo))
 
 // Ejercicio D1S2.1.1
+function attack(weaponName: string): string {
+    return weaponName
+}
+$("#output3").text(`Freddy ataca con una ${attack("cuchara de té")}`)
 
 // Ejercicio D1S2.1.2
+function jump(metters?: number): number {
+    if (metters) {
+        return metters
+    } else {
+        return 5
+    }
+}
+$("#output4").text(`Freddy salta ${jump()} metros`)
 
 // Ejercicio D1S2.2.1
+class Nightmare {
+    name: string
+
+    public constructor(name: string) {
+        this.name = name
+    }
+
+    scream() {
+        return `${this.name.toUpperCase()} está aquí!`
+    }
+}
+
+const myNightmare = new Nightmare("Un peruano")
+$("#output5").text(myNightmare.scream())
 
 // Ejercicio D1S2.2.2
+class AnotherNightmare {
+    name: string
+
+    public constructor(name: string) {
+        this.name = name
+    }
+
+    scream = (): string => {
+        return `${this.name.toUpperCase()} está aquí!`
+    }
+}
+const myAnotherNightmare = new AnotherNightmare("Un chileno")
+$("#output6").text(myAnotherNightmare.scream())
