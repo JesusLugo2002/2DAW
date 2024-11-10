32c4f6cb11f84ba0dce94844bf2ea2c2e4953404
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-10 16:20:57 +0000

    Se añade el ejer1 de la tarea de Vue

---
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea01/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import Ejemplo1 from './components/Ejemplo1.vue';
+</script>
+
+<template>
+  <Ejemplo1/>
+  <hr>
+  
+</template>
+
---
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea01/src/components/Ejemplo1.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const num = Math.floor((Math.random() * 6) + 1)
+</script>
+
+<template>
+    <h1>Ejercicio 01</h1>
+    <h2>Hello world!</h2>
+    <p>This is my random number -> {{ num }}</p>
+</template>
+
+<style>
+</style>
