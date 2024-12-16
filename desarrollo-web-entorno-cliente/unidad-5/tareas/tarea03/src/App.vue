3d06f35b020123d5b43e7297379d876c80efee97
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-12-16 21:14:07 +0000

    Initial config to DEW Vue homework

---
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea03/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Gestión de lista de tareas</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea04/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Tarea de directivas - IT</h1>
+</template>
+
+<style scoped>
+
+</style>
