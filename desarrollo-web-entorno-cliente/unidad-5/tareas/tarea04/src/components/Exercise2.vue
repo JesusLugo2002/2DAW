a82eab0f8cf9680172392fcbeb7a850543814875
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-01-01 23:19:43 +0000

    Finish (?) the exercise 2

--- desarrollo-web-entorno-cliente/unidad-5/tareas/tarea04/src/components/Exercise2.vue
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea04/src/components/Exercise2.vue
@@ -1,3 +1,31 @@
+<script setup>
+    import { ref } from 'vue';
+    import Balloon from './includes/Balloon.vue';
+
+    const balloonCount = ref(1)
+    const showingMessage = ref(false)
+
+    const switchMessage = () => {
+        showingMessage.value = !showingMessage.value
+    }
+</script>
+
 <template>
-    <h1>Exercise 2</h1>
+    <div class="container">
+        <h1 class="mb-4">La casa flotante de los globos rojos</h1>
+        <div class="text-center">
+            <h3 class="text-secondary">¡Elige la cantidad de globos!</h3>
+            <input type="range" id="balloon-slider" min="1" max="20" v-model.number="balloonCount" class="form-range">
+            <h3 class="text-secondary">Hay {{ balloonCount }} globos.</h3>
+        </div>
+        
+        
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <div id="balloon-container" class="d-flex justify-content-center flex-wrap w-50 mt-5">
+                <Balloon v-for="num in balloonCount" :key="num"/>
+            </div>
+            <button v-if="balloonCount >= 10" class="btn btn-danger mt-5" @click="switchMessage">¿Mensaje?</button>
+            <h5 v-if="showingMessage" class="text-danger mt-5">¡Todos flotan aquí!</h5>
+        </div>
+    </div>
 </template>
---
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea04/src/components/includes/Balloon.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="balloon"></div>
+</template>
+
+<style scoped>
+    .balloon {
+        width: 50px; 
+        height: 50px; 
+        background-color: red;
+        border-radius: 50%; 
+        margin: 10px; 
+        display: inline-block;
+    }
+</style>
