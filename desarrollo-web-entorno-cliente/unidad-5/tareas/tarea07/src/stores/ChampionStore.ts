bc2215698a684e9005d1319cf1fdeaab83cc71fa
JesusLugo2002 <150654444+JesusLugo2002@users.noreply.github.com>
2025-01-15 12:58:19 +0000

    Prototype ChampionStore.ts

--- desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/stores/ChampionStore.ts
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/stores/ChampionStore.ts
@@ -26,3 +26,38 @@ export const useChampionStore = defineStore('champion', {
     }
   }
 })
+
+export const useChampionStore = defineStore('champion', () => {
+const currentChampion = ref(null)
+const experience = ref(0)
+
+const stats = reactive({
+hp: 0,
+mp: 0,
+lvl: 1
+})
+
+function increaseExp(value: int) {
+experience.value += 10;
+}
+
+function decreaseExp(value: int) {
+experience.value -= 10;
+}
+
+const power = computed(() => {
+return (stats.hp + stats.mp) * stats.lvl
+}
+
+# Pending watch(experience) to level up
+
+return {
+currentChampion,
+experience,
+stats,
+increaseExp,
+decreaseExp,
+power
+}
+
+})
