d948db397b7ce7f175cce6aa42923d0382b2de3a
JesusLugo2002 <150654444+JesusLugo2002@users.noreply.github.com>
2025-01-15 13:29:30 +0000

    Prototype ChampionSelected.vue

--- desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/components/ChampionSelected.vue
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/components/ChampionSelected.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { useChampionStore } from '@/stores/ChampionStore';
-
-const currentChampion = useChampionStore()
-
+const props = defineProps(['championStore'])
 </script>
 
 <template>
-    <p class="text-center h3">The selected champion is <span class="text-success">{{ currentChampion.name }}</span></p>
+    <p class="text-center h3">The selected champion is <span class="text-success">{{ championStore.currentChampion.name }}</span></p>
     <div class="row d-flex justify-content-around">
-        <button class="col-4 btn btn-danger fw-bold" @click="currentChampion.decreaseExp">- Experience</button>
-        <button class="col-4 btn btn-primary fw-bold" @click="currentChampion.increaseExp">+ Experience</button>
+        <button class="col-4 btn btn-danger fw-bold" @click="championStore.decreaseExp">- Experience</button>
+        <button class="col-4 btn btn-primary fw-bold" @click="championStore.increaseExp">+ Experience</button>
     </div>
 </template>
