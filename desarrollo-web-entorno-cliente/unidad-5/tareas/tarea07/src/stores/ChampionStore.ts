cdd8a1d043fd1535244809cd8e82ed6233248372
JesusLugo2002 <150654444+JesusLugo2002@users.noreply.github.com>
2025-01-15 13:20:53 +0000

    Prototype ChampionStore.ts

--- desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/stores/ChampionStore.ts
+++ desarrollo-web-entorno-cliente/unidad-5/tareas/tarea07/src/stores/ChampionStore.ts
@@ -1,32 +1,6 @@
 import { defineStore } from 'pinia'
 import { ref, reactive, computed, watch } from 'vue'
 
-export const useChampionStore = defineStore('champion', {
-  state: () => ({
-    currentChampion: ref(null),
-    experience: ref(0),
-    stats: reactive({
-      hp: 0,
-      mp: 0,
-      lvl: 1
-    }),
-  }),
-  getters: {},
-  actions: {
-    increaseExp(): void {
-      this.experience += 10
-    },
-    decreaseExp(): void {
-      this.experience -= 10
-    },
-  },
-  computed: {
-    power() {
-      return (this.stats.hp + this.stats.mp) * this.stats.lvl
-    }
-  }
-})
-
 export const useChampionStore = defineStore('champion', () => {
 const currentChampion = ref(null)
 const experience = ref(0)
